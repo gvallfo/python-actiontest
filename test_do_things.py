2865c312b070821b41d1459201421049f6741244
Göran Vällfors <goran.vallfors@trimble.se>
2022-05-12 10:13:55 +0200

    Fixed some linting errors for pytest fixtures

--- test_do_things.py
+++ test_do_things.py
@@ -1,29 +1,29 @@
-'''
+"""
 Test for the do_things module
-'''
+"""
 import pytest
 import do_things
 
 
-@pytest.fixture
-def text():
-    '''
+@pytest.fixture(name="abc_text")
+def fixture_abc_text():
+    """
     A text used in test
-    '''
+    """
     return "Apa Boll Citron"
 
 
-@pytest.fixture
-def text_lower():
-    '''
+@pytest.fixture(name="abc_text_lower")
+def fixture_abc_text_lower():
+    """
     The test text as lower case
-    '''
+    """
     return "apa boll citron"
 
 
-def test_format_some_text_to_lower_case(text, text_lower):
-    '''
+def test_format_some_text_to_lower_case(abc_text, abc_text_lower):
+    """
     Verify output from format_some_text_to_lower_case()
-    '''
-    formatted_text = do_things.format_some_text_to_lower_case(text)
-    assert formatted_text == text_lower
+    """
+    formatted_text = do_things.format_some_text_to_lower_case(abc_text)
+    assert formatted_text == abc_text_lower
